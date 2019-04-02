fa7e1cc1cc7e9cc0c2a879ef835dccc5c78ebcbf
shaijuk923 <shaijuk923@gmail.com>
2019-04-02 14:14:56 +0530

    secondprogram

---
+++ hello2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int fact=1,i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+printf("factorila of &n is",&fact);
+getch();
+}
